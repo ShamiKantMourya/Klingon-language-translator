cac1e5e58624c14cf57e84cc57bdde2a0838a4e3
BlackxJoker <88420924+BlackxJoker@users.noreply.github.com>
2022-10-28 00:04:25 +0530

    klingon api

--- klingon.js
+++ klingon.js
@@ -1,3 +1,21 @@
 const outputText = document.querySelector('#output');
 const inputText = document.querySelector('#input');
 const translateBtn = document.querySelector('#btn');
+
+
+var url = "https://api.funtranslations.com/translate/klingon.json";
+
+function getTranslation(text) {
+    return url + "?" + "text=" + text;
+}
+
+function clickHandler() {
+    var inputArea = inputText.value;
+    fetch(getTranslation(inputArea))
+        .then(response => response.json())
+        .then(json => {
+            outputText.innerText = json.contents.translated;
+        })
+}
+
+translateBtn.addEventListener("click", clickHandler);
